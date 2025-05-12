4ebfbc691867eebc0493b402c4c63ad6a2b362ac
amulshaturia <97428617+amulshaturia@users.noreply.github.com>
2025-05-13 00:42:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const path = require('path')
 app.use(express.json());
 app.use(cors())
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 dotenv.config()
 app.use(express.json({ limit: '50mb' }));
